737116d2c053f206d424c94765cf10972cb083ab
Matias Rodriguez <matiasrodriguezb@gmail.com>
2024-04-02 10:27:13 -0300

    arreglo authslice

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -2,22 +2,31 @@ import { createSlice } from "@reduxjs/toolkit";
 
 /* /* Este fragmento de código está creando una porción del estado de Redux para manejar datos relacionados con la autenticación y también 
 se maneja lo relacionado al almacenamiento de la imagen de perfil y la imagen tomada con la cámara del celular. */ 
+
+
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
     value: {
-      user: true,
+      user: null,
       token: null,
       imageCamera: null,
-      localId: "Pepitoasdasdazz1414wfgsfsf",
+      localId: null,
       profileImage: null,
+      location: {
+        latitude: null,
+        longitude: null,
+        address: null,
+      },
     },
   },
   reducers: {
     setUser: (state, action) => {
       state.value = {
-        user: action.payload.data.email,
-        token: action.payload.data.idToken,
+        ...state.value,
+        user: action.payload.email,
+        token: action.payload.idToken,
+        localId: action.payload.localId,
       };
     },
     clearUser: (state) => (state.value = { user: null, token: null }),
@@ -33,9 +42,40 @@ export const authSlice = createSlice({
         profileImage: action.payload,
       };
     },
+    setUserLocation: (state, action) => {
+      state.value = {
+        ...state.value,
+        location: {
+          latitude: action.payload.latitude,
+          longitude: action.payload.longitude,
+          address: action.payload.address,
+        },
+      };
+    },
+    logout: (state) => {
+      state.value = {
+        user: null,
+        token: null,
+        imageCamera: null,
+        localId: null,
+        profileImage: null,
+        location: {
+          latitude: null,
+          longitude: null,
+          address: null,
+        },
+      };
+    },
   },
 });
 
-export const { setUser, clearUser, setCameraImage, setProfileImage } = authSlice.actions;
+export const {
+  setUser,
+  clearUser,
+  setCameraImage,
+  setProfileImage,
+  setUserLocation,
+  logout,
+} = authSlice.actions;
 
 export default authSlice.reducer;
